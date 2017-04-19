cb8c304021b3710e8a7082aa3e5347074524e0eb
chenli <chenli@192.168.0.117>
2017-04-19 22:13:00 +0800

    refactor

--- Project/src/main/java/evolution/controller/AnotherController.java
+++ Project/src/main/java/evolution/controller/AnotherController.java
@@ -1,11 +1,13 @@
 package evolution.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import evolution.controller.dto.RequestDto;
 import evolution.controller.dto.ResponseDto;
@@ -32,6 +34,13 @@ public class AnotherController {
 	
 	@PostMapping("/evolution/8")
 	public ResponseDto evolution8(@RequestBody RequestDto dto) {
+		return null;
+	}
+	
+	@GetMapping("/evolution/9")
+	public ResponseDto evolution9(@RequestParam("name") String name, 
+			@RequestParam(name = "gender", required = true) String gender,
+			@RequestParam(name = "hobby", required = false) String hobby) {
 		return null;
 	}
 }
