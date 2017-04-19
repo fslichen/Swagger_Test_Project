e8da7120fe4f45b6e4dace2704064dd5eee220e2
chenli <chenli@192.168.0.117>
2017-04-19 22:36:14 +0800

    refactor

--- Project/src/test/java/evolution/SwaggerTest.java
+++ Project/src/test/java/evolution/SwaggerTest.java
@@ -6,8 +6,7 @@ public class SwaggerTest {
 	@Test
 	public void test() {
 		String projectPath = "/Users/chenli/Desktop/Playground/Git/Test_Project/Project/src/main/java/evolution";
-		String projectBasePath = "/Users/chenli/Desktop/Playground/Git/Test_Project/Project/src/main/java";
 		String destinationPath = "/Users/chenli/Desktop/Swagger_Documents";
-		SwaggerFactory.swaggers(projectPath, projectBasePath, destinationPath);
+		SwaggerFactory.swaggers(projectPath, destinationPath);
 	}
 }
