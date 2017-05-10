a8ef8453ddad378c5bd3d8d39d86e147a70ec3ad
chenli <chenli@192.168.43.24>
2017-05-10 19:32:20 +0800

    refactor

--- Project/src/main/java/com/evolution/controller/dto/example/properties/AnotherDto.java
+++ Project/src/main/java/com/evolution/controller/dto/example/properties/AnotherDto.java
@@ -5,17 +5,26 @@ import evolution.annotation.ApiExamples;
 public class AnotherDto {
 	@ApiExamples(keys = {"ExamplePropertiesController.post"}, stringValues = {"M"})
 	private String gender;
+	private String hobby;
 
 	public String getGender() {
 		return gender;
+	}
+
+	public String getHobby() {
+		return hobby;
 	}
 
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
 	@Override
 	public String toString() {
-		return "GammaDto [gender=" + gender + "]";
+		return "AnotherDto [gender=" + gender + ", hobby=" + hobby + "]";
 	}
 }
