72a5a7f7239f9cfd33dc5e445dec325284a775fa
chenli <chenli@192.168.43.24>
2017-05-10 17:35:50 +0800

    new demos

--- Project/src/main/java/com/evolution/controller/ExamplePropertiesController.java
+++ Project/src/main/java/com/evolution/controller/ExamplePropertiesController.java
@@ -1,5 +1,7 @@
 package com.evolution.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ import com.evolution.controller.dto.example.properties.AnyDto;
 @RestController
 public class ExamplePropertiesController {
 	@PostMapping("/example/properties/post")
-	public AnyDto post(@RequestBody AnyDto anyDto) {
+	public AnyDto post(@RequestBody List<AnyDto> anyDtos) {
 		return null;
 	}
 }
