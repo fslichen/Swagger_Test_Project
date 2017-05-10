8c4213bdb22757dca4e101312c22f07a56942d89
chenli <chenli@192.168.43.24>
2017-05-10 15:49:39 +0800

    refactor

--- Project/src/test/java/evolution/ExamplePropertiesTest.java
+++ Project/src/test/java/evolution/ExamplePropertiesTest.java
@@ -4,26 +4,23 @@ import java.util.Map;
 
 import org.junit.Test;
 
-import com.evolution.controller.AlphaController;
 import com.evolution.controller.ExamplePropertiesController;
 import com.evolution.controller.dto.example.properties.AnyDto;
 
+import evolution.dto.DefaultSwagger;
 import evolution.dto.Property;
 
 public class ExamplePropertiesTest {
 	@Test
-	public void testExampleProperties() throws NoSuchMethodException, SecurityException {
-		Map<String, Property> map = SwaggerFactory.apiExampleProperties(AlphaController.class, 
-				AlphaController.class.getDeclaredMethod("post", AnyDto.class), 
+	public void testApiExampleProperties() throws NoSuchMethodException, SecurityException {
+		Map<String, Property> map = SwaggerFactory.apiExampleProperties(ExamplePropertiesController.class, 
+				ExamplePropertiesController.class.getDeclaredMethod("post", AnyDto.class), 
 				AnyDto.class);
 		System.out.println(map);
 	}
 	
 	@Test
-	public void test() throws NoSuchMethodException, SecurityException {
-		Map<String, Property> map = SwaggerFactory.apiExampleProperties(ExamplePropertiesController.class, 
-				ExamplePropertiesController.class.getDeclaredMethod("post", AnyDto.class), 
-				AnyDto.class);
-		System.out.println(map);
+	public void testSwagger() {
+		SwaggerFactory.swagger(ExamplePropertiesController.class, "", new DefaultSwagger());
 	}
 }
